b6bd1293c4c8c3254ecdba6ee827bc7bc924699f
Pahladine <adamcaza76@gmail.com>
2025-08-08 13:01:16 -0400

    Initial commit - Pages Router setup with ForgeHero v2.0 base

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+﻿import "@/styles/globals.css";
+import { StoryProvider } from "@/context/StoryContext";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <StoryProvider>
+      <Component {...pageProps} />
+    </StoryProvider>
+  );
+}
+
+export default MyApp;
